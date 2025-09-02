aa614976fc07f40d9f01336cfda9c5c7db7200c4
Wisnu <wisnulintang111@gmail.com>
2025-09-02 16:39:41 +0700

    Commit No. 3 – Fix application crash when user input wrong color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/Result_Activity.kt
+++ app/src/main/java/com/example/lab_week_02_b/Result_Activity.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_02_b
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,8 @@ import androidx.core.graphics.toColorInt
 class ResultActivity : AppCompatActivity() {
 
     companion object {
-        private const val COLOR_KEY = "COLOR_KEY"
+        const val COLOR_KEY = "COLOR_KEY"
+        const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +23,21 @@ class ResultActivity : AppCompatActivity() {
 
         val colorCode = intent.getStringExtra(COLOR_KEY)
 
-        colorCode?.let {
-            try {
-                backgroundScreen.setBackgroundColor(("#$it").toColorInt())
-                resultMessage.text = getString(
-                    R.string.color_code_result_message,
-                    it.uppercase()
-                )
-            } catch (_: IllegalArgumentException) {
-                // fallback if somehow invalid code passed
-                backgroundScreen.setBackgroundColor(android.graphics.Color.WHITE)
-                resultMessage.text = getString(R.string.color_code_input_invalid)
-            }
-        } ?: run {
-            // if null, show fallback
-            backgroundScreen.setBackgroundColor(android.graphics.Color.WHITE)
-            resultMessage.text = getString(R.string.color_code_input_empty)
+        if (colorCode.isNullOrBlank()) {
+            setResult(RESULT_OK, Intent().putExtra(ERROR_KEY, true)) // no redundant qualifier
+            finish()
+            return
+        }
+
+        try {
+            backgroundScreen.setBackgroundColor("#$colorCode".toColorInt())
+            resultMessage.text = getString(
+                R.string.color_code_result_message,
+                colorCode.uppercase()
+            )
+        } catch (_: IllegalArgumentException) {
+            setResult(RESULT_OK, Intent().putExtra(ERROR_KEY, true)) // no redundant qualifier
+            finish()
         }
     }
 }
